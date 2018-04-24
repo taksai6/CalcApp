3ee169d44b15d30280a0b513dce561910cc269fc
taksai6 <ta93_5@yahoo.co.jp>
2018-04-24 16:49:41 +0900

    Calc

--- app/src/main/java/jp/techacademy/takumi/nakamura/calcapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/takumi/nakamura/calcapp/MainActivity.java
@@ -3,18 +3,15 @@ package jp.techacademy.takumi.nakamura.calcapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ActionMode;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    TextView mTextView1;
     EditText mEditText1;
 
-    TextView mTextView2;
     EditText mEditText2;
 
 
@@ -35,10 +32,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button button4 = (Button) findViewById(R.id.button4);
         button4.setOnClickListener(this);
 
-        mTextView1 = (TextView) findViewById(R.id.textView);
+
         mEditText1 = (EditText) findViewById(R.id.editText1);
 
-        mTextView2 = (TextView) findViewById(R.id.textView);
+
         mEditText2 = (EditText) findViewById(R.id.editText2);
 
     }
@@ -46,31 +43,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         Intent intent = new Intent(this, SecondActivity.class);
-        double value1 = Double.parseDouble(mEditText1.getText().toString());
-        double value2 = Double.parseDouble(mEditText2.getText().toString());
-        intent.putExtra("VALUE2", value1);
-        intent.putExtra("VALUE2", value2);
+        try {
+            double value1 = Double.parseDouble(mEditText1.getText().toString());
+            double value2 = Double.parseDouble(mEditText2.getText().toString());
+            intent.putExtra("VALUE2", value1);
+            intent.putExtra("VALUE2", value2);
+            if (v.getId() == R.id.button1) {
+                intent.putExtra("result", value1 + value2);
 
-        if (v.getId() == R.id.button1) {
-            intent.putExtra("result", value1 + value2);
+            } else if (v.getId() == R.id.button2) {
+                intent.putExtra("result", value1 - value2);
 
-        } else if (v.getId() == R.id.button2) {
-            intent.putExtra("result", value1 - value2);
+            } else if (v.getId() == R.id.button3) {
+                intent.putExtra("result", value1 * value2);
 
-        } else if (v.getId() == R.id.button3) {
-            intent.putExtra("result", value1 * value2);
+            } else if (v.getId() == R.id.button4 && value2 == 0) {
+                Toast.makeText(MainActivity.this,
+                        "0で割らないでください",
+                        Toast.LENGTH_LONG).show();
 
-        } else if (v.getId() == R.id.button4) {
-            intent.putExtra("result", value1 / value2);
+            } else if (v.getId() == R.id.button4) {
+                intent.putExtra("result", value1 / value2);
+            }
+        } catch (NumberFormatException e) {
+            Toast.makeText(MainActivity.this,
+                    "数字を入力してください",
+                    Toast.LENGTH_LONG).show();
 
 
         }
+
 
         startActivity(intent);
 
 
     }
-
-
 }
+
+
 
